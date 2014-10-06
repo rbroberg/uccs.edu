678e07ef105f53bc16532e1a32e09a0d96ad5b50
unknown <rbroberg@CHAPEL02.uccs.edu>
2014-10-05 19:17:05 -0600

    multiproc

---
+++ cs5870/hw/iterateModel.jl
@@ -0,0 +1,14 @@
+# independent inner loop for training model on one digit/letter
+# -----------------------------------------------------------------------------
+function iterateModel(X, Y, wghts, bs, theta, c, nclass)
+	for i in 1:size(X)[1]
+		y_in=dot(vec(X[1,:]),vec(wghts[c,:]))+bs[c]; # inputs * weights + bias
+		y_out = int(sign(y_in)*int(abs(y_in)>theta)); # -1 < -theta else 1 > theta else 0
+		if !(y_out == 1 && mod(c,nclass) == Y[i])
+			y_bipolar = int(mod(c,nclass) == Y[i])*2-1;
+			bs[c] = bs[c] +  y_bipolar; # -1 for no match, +1 for match
+			wghts[c,:] = wghts[c,:] + y_bipolar*X[i,:];
+		end
+	end
+	return wghts[c,:], bs[c]
+end
