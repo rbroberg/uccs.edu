157041e689108dd671262e87142f2170d5b3006c
rbroberg <ronald.broberg@mdnt.com>
2014-10-07 12:20:53 -0600

    working parallelization

--- cs5870/hw/hw1d.jl
+++ cs5870/hw/hw1d.jl
@@ -18,7 +18,7 @@ csvdat = readcsv(IOBuffer(res.data));
 # data is 0-255 for gray valued images
 # use 70/30 for training/testing split
 
-idx=294;
+idx=int(42000 * .70);
 X_train=convert(Array{Float64,2},csvdat[2:idx,2:end]);
 Y_train=convert(Array{Int16,1},csvdat[2:idx,1]);
 X_test=convert(Array{Float64,2},csvdat[(idx+1):end,2:end]);
@@ -44,45 +44,88 @@ theta = 0.1;
 # -----------------------------------------------------------------------------
 # make training loop available to all threads
 # -----------------------------------------------------------------------------
-# require("/projects/uccs.edu/cs5870/hw/iterateModel.jl");
+require("/projects/uccs.edu/cs5870/hw/iterateModel.jl");
 
 # -----------------------------------------------------------------------------
-# train the model
+# model training parallelized on class
 # -----------------------------------------------------------------------------
-# parallel digits
-# -----------------------------------------------------------------------------
-for epochs in 1:10
-        println("epoch: ", epochs)
-		flush(STDOUT)
-	#@parallel for idx in 1:nclass
-	for idx in 1:nclass
-		weights[idx,:],bias[idx] = fitModel(X_train, Y_train, weights, bias, theta, idx, nclass);
-	end
+function pfit(f, X, Y, weights, bias, theta, idx, nclass)
+    np = nprocs()  # determine the number of processes available
+    i = 1
+    # function to produce the next work item from the queue.
+    # in this case it's just an index.
+    nextidx() = (idx=i; i+=1; idx)
+    @sync begin
+        for p=1:np
+            if p != myid() || np == 1
+                @async begin
+                    while true
+                        idx = nextidx()
+                        if idx > nclass
+                            break
+                        end
+                        weights[idx,:],bias[idx] = remotecall_fetch(p, f, X, Y, weights, bias, theta, idx, nclass)
+                    end
+                end
+            end
+        end
+    end
+    weights, bias
 end
+
+for epoch in 1:4
+	println("epoch: ", epoch)
+	flush(STDOUT)
+	weights, bias = pfit(fitModel, X_train, Y_train, weights, bias, theta, idx, nclass)
+end
+
+# -----------------------------------------------------------------------------
+# model prediction parallelized on class
+# -----------------------------------------------------------------------------
+function ppred(f, preds, X, weights, bias, nclass)
+    np = nprocs()  # determine the number of processes available
+    i = 1
+    # function to produce the next work item from the queue.
+    # in this case it's just an index.
+    nextidx() = (idx=i; i+=1; idx)
+    @sync begin
+        for p=1:np
+            if p != myid() || np == 1
+                @async begin
+                    while true
+                        idx = nextidx()
+                        if idx > nclass
+                            break
+                        end
+                        preds[:,idx] = remotecall_fetch(p, f, X, weights[idx,:],bias[idx])
+                    end
+                end
+            end
+        end
+    end
+    preds
+end
+
 
 # -----------------------------------------------------------------------------
 # run sanity check on training data
 # -----------------------------------------------------------------------------
 Y_preds=zeros(size(X_train)[1],nclass);
-Y_hat=zeros(size(X_train)[1]);
-#@parallel for c in 1:nclass
-for c in 1:nclass
-		Y_preds[:,c]=predictModel(X_train,weights[c,:],bias[c]);
-end
+Y_preds = ppred(predictModel, Y_preds, X_train, weights, bias, nclass);
 Y_hat = [int(mod(indmax(Y_preds[i,:]),nclass)) for i in 1:size(Y_preds)[1]];
-print(sum(Y_hat.==Y_train) / (size(Y_train)[1]))
-
+println(sum(Y_hat.==Y_train) / (size(Y_train)[1]))
+flush(STDOUT)
 
 # -----------------------------------------------------------------------------
 # run test
 # -----------------------------------------------------------------------------
 Y_preds=zeros(size(X_test)[1],nclass);
-Y_hat=zeros(size(X_test)[1]);
-#@parallel for c in 1:nclass
-for c in 1:nclass
-		Y_preds[:,c]=predictModel(X_test,weights[c,:],bias[c]);
-end
+Y_preds = ppred(predictModel, Y_preds, X_test, weights, bias, nclass);
 Y_hat = [int(mod(indmax(Y_preds[i,:]),nclass)) for i in 1:size(Y_preds)[1]];
-print(sum(Y_hat.==Y_test) / (size(Y_test)[1]))
+println(sum(Y_hat.==Y_test) / (size(Y_test)[1]))
+flush(STDOUT)
 
 
+# idx=.7, epoch=4, theta=0.1, .858, .844
+# idx=.7, epoch=10, theta=0.1, .875, .858
+# idx=.7, epoch=40, theta=0.1, .892, .870
