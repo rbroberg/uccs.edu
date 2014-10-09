2108000711ccbfb3b2d4ed612e44d1317bec2542
unknown <rbroberg@CHAPEL03.uccs.edu>
2014-10-08 20:30:15 -0600

    finishing pairwise model

--- cs5870/hw/digit_recog_nn_layer1_pairwise.jl
+++ cs5870/hw/digit_recog_nn_layer1_pairwise.jl
@@ -15,7 +15,6 @@ csvdat = readcsv(IOBuffer(res.data));
 # first column are digit labels
 # size is 42001 x 785
 # data is 0-255 for gray valued images
-idx
 X_train=convert(Array{Float64,2},csvdat[2:end,2:end]);
 Y_train=convert(Array{Int16,1},csvdat[2:end,1]);
 
@@ -89,8 +88,7 @@ function pfit(f, X, Y, weights, bias, theta, nclass)
     weights, bias
 end
 
-
-for epoch in 1:40
+for epoch in 1:2
 	println("epoch: ", epoch)
 	flush(STDOUT)
 	weights, bias = pfit(fitModelPairwise, X_train, Y_train, weights, bias, theta, nclass);
@@ -99,22 +97,23 @@ end
 # -----------------------------------------------------------------------------
 # model prediction parallelized on class
 # -----------------------------------------------------------------------------
-function ppred(f, preds, X, weights, bias, nclass)
+function ppred(f, preds, X, weights, bias, pairs)
     np = nprocs()  # determine the number of processes available
     i = 1
     # function to produce the next work item from the queue.
     # in this case it's just an index.
     nextidx() = (idx=i; i+=1; idx)
+	nimages=size(X)[1]
     @sync begin
         for p=1:np
             if p != myid() || np == 1
                 @async begin
                     while true
                         idx = nextidx()
-                        if idx > nclass
+                        if idx > nimages
                             break
                         end
-                        preds[:,idx] = remotecall_fetch(p, f, X, weights[idx,:],bias[idx])
+						preds[idx]= remotecall_fetch(p, f, X_test[idx,:], weights, bias, pairs)
                     end
                 end
             end
@@ -123,35 +122,18 @@ function ppred(f, preds, X, weights, bias, nclass)
     preds
 end
 
-# this provides a vector to get select all results relating to a single digit class 0-9 
-#  [ ', ', ', ', v, ', ', ', ', X, ', ', ', ', v, ', ', ', ', X, ', ', ', ', v, ', ', ', ', X, ', ', ', ', v, ', ', ', ', X, ', ', ', ', v ]
-d=[[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-   [-1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-   [ 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-   [ 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-   [ 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-   [ 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
-   [ 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0, 0, 0,-1, 0, 0, 0, 1, 1, 1, 0, 0, 0 ],
-   [ 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0, 0, 0,-1, 0, 0,-1, 0, 0, 1, 1, 0 ],
-   [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0, 0, 0,-1, 0, 0,-1, 0,-1, 0, 1 ],
-   [ 0, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0, 0, 0,-1, 0, 0,-1, 0,-1,-1 ]];
-d=reshape(d,45,10);
-
 # -----------------------------------------------------------------------------
 # run sanity check on training data
 # -----------------------------------------------------------------------------
-Y_hat=zeros(size(Y_train)[1]);
-for k in 1:size(Y_train)[1]
-	x=[dot(vec(X_train[k,:]),vec(weights[pairs[a][1]+1,pairs[a][2]+1,:]))+bias[pairs[a][1]+1,pairs[a][2]+1] for a in 1:npairs]
-	x=convert(Array{Float64,1},x)
-	Y_hat[k]=indmax([dot(x,d[:,b]) for b in 1:10])-1
-end
-println(sum(Y_hat.==Y_train) / (size(Y_train)[1]))
-flush(STDOUT)
+#Y_hat=zeros(size(Y_train)[1]);
+#for k in 1:size(Y_train)[1]
+#	x=[dot(vec(X_train[k,:]),vec(weights[pairs[a][1]+1,pairs[a][2]+1,:]))+bias[pairs[a][1]+1,pairs[a][2]+1] for a in 1:npairs]
+#	x=convert(Array{Float64,1},x)
+#	Y_hat[k]=indmax([dot(x,d[:,b]) for b in 1:10])-1
+#end
+#println(sum(Y_hat.==Y_train) / (size(Y_train)[1]))
+#flush(STDOUT)
 
-# -----------------------------------------------------------------------------
-# run test
-# -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # run test
 # -----------------------------------------------------------------------------
@@ -163,16 +145,20 @@ X_test=convert(Array{Float64,2},csvdat[2:end,1:end]);
 # normalize to bipolar -1,1
 X_test=(X_test/255)*2-1;
 
-Y_hat=zeros(size(Y_test)[1]);
-for k in 1:size(Y_test)[1]
-	x=[dot(vec(X_test[k,:]),vec(weights[pairs[a][1]+1,pairs[a][2]+1,:]))+bias[pairs[a][1]+1,pairs[a][2]+1] for a in 1:npairs]
-	x=convert(Array{Float64,1},x)
-	Y_hat[k]=indmax([dot(x,d[:,b]) for b in 1:10])-1
-end
-println(sum(Y_hat.==Y_test) / (size(Y_test)[1]))
+Y_hat=zeros(size(X_test)[1]);
+Y_hat=ppred(predictModelPairwise, Y_hat, X_test, weights, bias, pairs)
+
+# -----------------------------------------------------------------------------
+# kaggle submission
+# -----------------------------------------------------------------------------
+rows=[i for i in 1:size(Y_hat)[1]];
+y=convert(Array{Int16,2},hcat(rows,Y_hat))
+writedlm("nn.csv", y, ",") 
+println("manual enter 'ImageID,Label' as first row")
 flush(STDOUT)
 
 quit()
 
 # split 0.8, epoch 10, theta 0.1, .856, .853
+# split 1.0, epoch 2, theta 0.1, .852, .---
 
