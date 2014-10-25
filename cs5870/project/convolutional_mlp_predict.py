0479ab7b45a52f4b8273dba6f9a444858e201671
unknown <rbroberg@CHAPEL02.uccs.edu>
2014-10-25 12:50:01 -0600

    tab to space

--- cs5870/project/convolutional_mlp_predict.py
+++ cs5870/project/convolutional_mlp_predict.py
@@ -51,32 +51,32 @@ features=["ac","var","ent","ent2","skew","kurt"]
 
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
-	
+    
     def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
         """
         Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-		
+        
         :type rng: numpy.random.RandomState
         :param rng: a random number generator used to initialize weights
-		
+        
         :type input: theano.tensor.dtensor4
         :param input: symbolic image tensor, of shape image_shape
-		
+        
         :type filter_shape: tuple or list of length 4
         :param filter_shape: (number of filters, num input feature maps,
                               filter height, filter width)
-		
+        
         :type image_shape: tuple or list of length 4
         :param image_shape: (batch size, num input feature maps,
                              image height, image width)
-		
+        
         :type poolsize: tuple or list of length 2
         :param poolsize: the downsampling (pooling) factor (#rows, #cols)
         """
-		
+        
         assert image_shape[1] == filter_shape[1]
         self.input = input
-		
+        
         # there are "num input feature maps * filter height * filter width"
         # inputs to each hidden unit
         fan_in = numpy.prod(filter_shape[1:])
@@ -94,11 +94,11 @@ class LeNetConvPoolLayer(object):
             ),
             borrow=True
         )
-		
+        
         # the bias is a 1D tensor -- one bias per output feature map
         b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
         self.b = theano.shared(value=b_values, borrow=True)
-		
+        
         # convolve input feature maps with filters
         conv_out = conv.conv2d(
             input=input,
@@ -106,20 +106,20 @@ class LeNetConvPoolLayer(object):
             filter_shape=filter_shape,
             image_shape=image_shape
         )
-		
+        
         # downsample each feature map individually, using maxpooling
         pooled_out = downsample.max_pool_2d(
             input=conv_out,
             ds=poolsize,
             ignore_border=True
         )
-		
+        
         # add the bias term. Since the bias is a vector (1D array), we first
         # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
         # thus be broadcasted across mini-batches and feature map
         # width & height
         self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-		
+        
         # store parameters of this layer
         self.params = [self.W, self.b]
 
@@ -145,21 +145,21 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=20,
     """
 
     rng = numpy.random.RandomState(23455)
-
-	datasets = load_data(cases[0][0],cases[0][1],cases[0][2],cases[0][3],["cc"],0.5)
-	
-	train_set_x, train_set_y = datasets[0]
-	valid_set_x, valid_set_y = datasets[1]
-	test_set_x, test_set_y = datasets[2]
-	
+    
+    datasets = load_data(cases[0][0],cases[0][1],cases[0][2],cases[0][3],["cc"],0.5)
+    
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+    
     # compute number of minibatches for training, validation and testing
-	n_train_batches = train_set_x.shape[0] 
-	n_valid_batches = valid_set_x.shape[0]
-	n_test_batches = test_set_x.shape[0]
-	n_train_batches /= batch_size
-	n_valid_batches /= batch_size
-	n_test_batches /= batch_size
-	
+    n_train_batches = train_set_x.shape[0] 
+    n_valid_batches = valid_set_x.shape[0]
+    n_test_batches = test_set_x.shape[0]
+    n_train_batches /= batch_size
+    n_valid_batches /= batch_size
+    n_test_batches /= batch_size
+    
     # allocate symbolic variables for the data
     index = T.lscalar()  # index to a [mini]batch
 
@@ -172,38 +172,38 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=20,
     # BUILD ACTUAL MODEL #
     ######################
     print '... building the model'
-	
+    
     # Reshape matrix of rasterized images of shape (batch_size, 28 * 28)
     # to a 4D tensor, compatible with our LeNetConvPoolLayer
     # (28, 28) is the size of MNIST images.
-	d = reshape_crosscorr(train_set_x[1,:]).shape[1]
-	layer0_input = x.reshape((batch_size, 1, d, d))
+    d = reshape_crosscorr(train_set_x[1,:]).shape[1]
+    layer0_input = x.reshape((batch_size, 1, d, d))
 
     # Construct the first convolutional pooling layer:
     # filtering reduces the image size to (28-5+1 , 28-5+1) = (24, 24)
     # maxpooling reduces this further to (24/2, 24/2) = (12, 12)
     # 4D output tensor is thus of shape (batch_size, nkerns[0], 12, 12)
-	
-	nb = 2 # neighborhood was original 5, now 2
-	layer0 = LeNetConvPoolLayer(
-		rng,
-		input=layer0_input,
-		image_shape=(batch_size, 1, d, d),
-		filter_shape=(nkerns[0], 1, nb, nb),
-		poolsize=(2, 2)
-	)
+    
+    nb = 2 # neighborhood was original 5, now 2
+    layer0 = LeNetConvPoolLayer(
+        rng,
+        input=layer0_input,
+        image_shape=(batch_size, 1, d, d),
+        filter_shape=(nkerns[0], 1, nb, nb),
+        poolsize=(2, 2)
+    )
 
     # Construct the second convolutional pooling layer
     # filtering reduces the image size to (12-5+1, 12-5+1) = (8, 8)
     # maxpooling reduces this further to (8/2, 8/2) = (4, 4)
     # 4D output tensor is thus of shape (nkerns[0], nkerns[1], 4, 4)
-	layer1 = LeNetConvPoolLayer(
-		rng,
-		input=layer0.output,
-		image_shape=(batch_size, nkerns[0], d/2, d/2),
-		filter_shape=(nkerns[1], nkerns[0], 4, 4),
-		poolsize=(2, 2)
-	)
+    layer1 = LeNetConvPoolLayer(
+        rng,
+        input=layer0.output,
+        image_shape=(batch_size, nkerns[0], d/2, d/2),
+        filter_shape=(nkerns[1], nkerns[0], 4, 4),
+        poolsize=(2, 2)
+    )
 
     # the HiddenLayer being fully-connected, it operates on 2D matrices of
     # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
@@ -212,13 +212,13 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=20,
     layer2_input = layer1.output.flatten(2)
 
     # construct a fully-connected sigmoidal layer
-	layer2 = HiddenLayer(
-		rng,
-		input=layer2_input,
-		n_in=nkerns[1] * 4 * 4,
-		n_out=batch_size,
-		activation=T.tanh
-	)
+    layer2 = HiddenLayer(
+        rng,
+        input=layer2_input,
+        n_in=nkerns[1] * 4 * 4,
+        n_out=batch_size,
+        activation=T.tanh
+    )
 
     # classify the values of the fully-connected sigmoidal layer
     layer3 = LogisticRegression(input=layer2.output, n_in=batch_size, n_out=10)
@@ -227,23 +227,23 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=20,
     cost = layer3.negative_log_likelihood(y)
 
     # create a function to compute the mistakes that are made by the model
-	test_model = theano.function(
-		[index],
-		layer3.errors(y),
-		givens={
-			x: test_set_x[index * batch_size: (index + 1) * batch_size],
-			y: test_set_y[index * batch_size: (index + 1) * batch_size]
-		}
-	)
+    test_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+            y: test_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
 
-	validate_model = theano.function(
-		[index],
-		layer3.errors(y),
-		givens={
-			x: valid_set_x[index * batch_size: (index + 1) * batch_size],
-			y: valid_set_y[index * batch_size: (index + 1) * batch_size]
-		}
-	)
+    validate_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
 
     # create a list of all model parameters to be fit by gradient descent
     params = layer3.params + layer2.params + layer1.params + layer0.params
@@ -261,15 +261,15 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=20,
         for param_i, grad_i in zip(params, grads)
     ]
 
-	train_model = theano.function(
-		[index],
-		cost,
-		updates=updates,
-		givens={
-			x: train_set_x[index * batch_size: (index + 1) * batch_size],
-			y: train_set_y[index * batch_size: (index + 1) * batch_size]
-		}
-	)
+    train_model = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
     # end-snippet-1
 
     ###############
--- cs5870/project/lenet_util.py
+++ cs5870/project/lenet_util.py
@@ -1,29 +1,39 @@
 from numpy import genfromtxt
 
-def load_data(case,npre,ninter,ntest,features):
-	datadir="/data/www.kaggle.com/c/seizure-prediction/download/"
-	f=datadir+case+"/"+features[0]+".csv"
-	dat=genfromtxt(f, delimiter=',')
-	dat_train=dat[0:(npre+ninter),:]
-	dat_test=dat[(npre+ninter):,:]
-	dat_labels=np.hstack((np.ones(npre),np.zeros(ninter)))
-	if len(features)>1:
-		for n in range(1,len(features)):
-			f=datadir+case+"/"+features[n]+".csv"
-			dat=genfromtxt(f, delimiter=',')
-			dat_train=np.hstack((dat_train,dat[0:(npre+ninter),:]))
-			dat_test=np.hstack((dat_test,dat[(npre+ninter):,:]))
-	
-	return dat_train, dat_labels, dat_test
+def load_data(case,npre,ninter,ntest,features, split):
+    datadir="/data/www.kaggle.com/c/seizure-prediction/download/"
+    f=datadir+case+"/"+features[0]+".csv"
+    dat=genfromtxt(f, delimiter=',')
+    dat_train=dat[0:(npre+ninter),:]
+    dat_test=dat[(npre+ninter):,:]
+    dat_labels=np.hstack((np.ones(npre),np.zeros(ninter)))
+    if len(features)>1:
+        for n in range(1,len(features)):
+            f=datadir+case+"/"+features[n]+".csv"
+            dat=genfromtxt(f, delimiter=',')
+            dat_train=np.hstack((dat_train,dat[0:(npre+ninter),:]))
+            dat_test=np.hstack((dat_test,dat[(npre+ninter):,:]))
+    
+    # split has to be handled with weighting of pre/inter
+    mpre=int(round(npre*split))
+    minter=int(round(ninter*split))
+    #TODO: randomize draw
+    x_train=np.vstack((dat_train[0:mpre,:],dat_train[npre:(npre+minter),]))
+    x_valid=np.vstack((dat_train[mpre:npre,:],dat_train[(npre+minter):(npre+ninter),]))
+    y_train=np.hstack((np.ones(mpre),np.zeros(minter)))
+    y_valid=np.hstack((np.ones(npre-mpre),np.zeros(ninter-minter)))
+    x_test=dat_test
+    y_test=np.zeros(x_test.shape[0])
+    return [(x_valid, y_train), (x_valid, y_valid), (x_test,y_test)]
 
 def reshape_crosscorr(diag):
-	d=len(diag)
-	n=(int(round((240.*4.)**0.5))+1)/2 # form of soln for quad eqn
-	m=np.ones((n,n))
-	k=0
-	for i in range(n):
-		for j in range((i+1),n):
-			m[i,j]=diag[k]
-			m[j,i]=diag[k]
-			k=k+1
-	return m
+    d=len(diag)
+    n=(int(round((240.*4.)**0.5))+1)/2 # form of soln for quad eqn
+    m=np.ones((n,n))
+    k=0
+    for i in range(n):
+        for j in range((i+1),n):
+            m[i,j]=diag[k]
+            m[j,i]=diag[k]
+            k=k+1
+    return m
