3a9f92d41a3542e6f3bab70b4d2d8dad74898d2f
rbroberg <ronald.broberg@mdnt.com>
2015-05-06 15:59:08 -0600

    final

--- cs5860/src/bimi.py
+++ cs5860/src/bimi.py
@@ -5,54 +5,158 @@ import os.path
 import math
 
 def save_obj(obj, name ):
-    with open('../data/'+ name + '.pkl', 'wb') as f:
+    with open('../data/e100/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name ):
-    with open('../data/' + name + '.pkl', 'rb') as f:
+    with open('../data/e10/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
-
+# pab_a == pab?
+#def biMI(ab, pab_a, unigramcnt, bigramcnt):
 def biMI(ab, unigramcnt, bigramcnt):
     bfreq=sum(bigramcnt.values())
     ufreq=sum(unigramcnt.values())
     pa=1.*unigramcnt[ab.split(':')[0]]/ufreq
     pb=1.*unigramcnt[ab.split(':')[1]]/ufreq
     pab=1.*bigramcnt[ab]/bfreq
-    return math.log(pab/(pa*pb),2)
-
-def getBigrams(x):
-    tg=[]
-    for n in range(len(x)-1):
-         tg.append([x.iloc[n],x.iloc[n+1]])
-    return(tg)
+    #return math.log((pb*pa)/(pb),2)
+    #return math.log(pa,2) # promising
+    #return math.log((pab)/(pb),2)
+    #return math.log(pab * pa/pb,2)
+    #return math.log(pab_a * pa/pb,2)
+    return math.log(pab * pa/pb,2)
 
 def getBIMI(x):
-    return [biMI(':'.join(b),unigramcnt, bigramcnt) for b in x]
+    #return [biMI(b,1.*bigramcnt[b]/sum([bigramcnt[y] for y in x]),unigramcnt, bigramcnt) for b in x]
+    return [biMI(b,unigramcnt, bigramcnt) for b in x]
 
-def triMI(abc, unigramcnt, trigramcnt):
+def getNBP2(x):
+    return([math.log(1.*bigramcnt[b]/sum([bigramcnt[y] for y in x]),2) for b in x])
+
+def triMI(abc, unigramcnt, bigramcnt, trigramcnt):
     tfreq=sum(trigramcnt.values())
+    bfreq=sum(bigramcnt.values())
     ufreq=sum(unigramcnt.values())
-    pa=1.*unigramcnt[abc.split(':')[0]]/ufreq
-    pb=1.*unigramcnt[abc.split(':')[1]]/ufreq
-    pc=1.*unigramcnt[abc.split(':')[2]]/ufreq
-    pab=1.*trigramcnt[abc]/tfreq
-    return math.log(pab/(pa*pb*pc),2)
-
-def getTrigrams(x):
-    tg=[]
-    for n in range(len(x)-2):
-         tg.append([x.iloc[n],x.iloc[n+1],x.iloc[n+2]])
-    return(tg)
+    a=abc.split(':')[0]
+    b=abc.split(':')[1]
+    c=abc.split(':')[2]
+    ab=":".join([a,b])
+    ac=":".join([a,c])
+    bc=":".join([b,c])
+    pa=1.*unigramcnt[a]/ufreq
+    pb=1.*unigramcnt[b]/ufreq
+    pc=1.*unigramcnt[c]/ufreq
+    #pab=(1.*bigramcnt[ab]/bfreq)
+    pbc=(1.*bigramcnt[bc]/bfreq)
+    #pa_b=(1.*bigramcnt[ab]/bfreq)/pb
+    #pc_b=(1.*bigramcnt[ac]/bfreq)/pb
+    pabc=1.*trigramcnt[abc]/tfreq
+    #pcab=(1.*trigramcnt[ac]/tfreq)/pb_a
+    #return math.log(pab/(pa*pb*pc),2)
+    #return math.log(pa * pba * pcab ,2)
+    #return math.log(pa*pbc/pbc,2) # promising  
+    return math.log(pabc*pa/pbc,2)
 
 def getTRIMI(x):
-    return [triMI(':'.join(b),unigramcnt, trigramcnt) for b in x]
+    return [triMI(b,unigramcnt, bigramcnt, trigramcnt) for b in x]
 
-	
-unigramcnt=load_obj('tmp.udict')
-bigramcnt=load_obj('tmp.bcntdict')
-bdict=load_obj('tmp.bdict')
+# unigramcnt[unigramcnt.keys()[0]] ## 27
+unigramcnt=load_obj('tmp.unigramdict')
+
+# bigramcnt[bigramcnt.keys()[0]] ## 1
+bigramcnt=load_obj('tmp.bigramcnt')
+
+# bdict[bdict.keys()[0]] ## ['106582840:-1977522313', '106582840:-2146217110', ...
+bdict=load_obj('tmp.bigramdict')
+
+#
+hashpagedict=load_obj('tmp.hashpagedict')
+badpages=load_obj('tmp.badpages')
 
 bimi=[getBIMI(bdict[x]) for x in bdict.keys()]
-save_obj(bimi,"tmp.bimi2")
+#bimi=[getNBP2(bdict[x]) for x in bdict.keys()]
+#save_obj(bimi,"tmp.bimi2")
 
+#x=bdict[bdict.keys()[0]]
+#b=x[0]
+#biMI(b,unigramcnt, bigramcnt)
+
+# find negative MI bigrams
+# bimi[4] has 5 negative bigrams [5,6,14,18,21]
+#bdict[bdict.keys()[4]]
+
+# bdict[bdict.keys()[4]][5]
+## '1170436880:1663343820'
+# bdict[bdict.keys()[4]][6]
+## '1170436880:806440580'
+# bdict[bdict.keys()[4]][14]
+## '1170436880:569593942'
+# bdict[bdict.keys()[4]][18]
+## '1170436880:1894851142'
+# bdict[bdict.keys()[4]][21]
+## '1170436880:1369747478'
+#hashpagedict['1170436880']
+
+n=0
+m=0
+#bad=set()
+bad=0
+for i in range(len(bimi)):
+    for j in range(len(bimi[i])):
+        m=m+1
+        if hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]] == '/test/me':
+            print bimi[i]
+        if bimi[i][j] < -25.9:
+            n=n+1
+            if hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]] == '/test/me':
+                bad=bad+1
+            if hashpagedict[bdict[bdict.keys()[i]][j].split(':')[1]] == '/test/me':
+                bad=bad+1
+
+print bad,n,m,len(badpages)
+
+'''
+            #print(hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]] in badpages," >>> ",hashpagedict[bdict[bdict.keys()[i]][j].split(':')[1]] in badpages)
+            #if hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]] in badpages:
+                #bad.add(hashpagedict[bdict[bdict.keys()[i]][j].split(':')[1]])
+                #print(hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]]+" >>> "+hashpagedict[bdict[bdict.keys()[i]][j].split(':')[1]])
+                #print(hashpagedict[bdict[bdict.keys()[i]][j].split(':')[0]] in badpages," >>> ",hashpagedict[bdict[bdict.keys()[i]][j].split(':')[1]] in badpages)
+
+
+
+
+
+# sum([v==1 for v in bv]) ## 6484 unique bigrams in 8871 bigrams
+
+
+# unigramcnt[unigramcnt.keys()[0]] ## 27
+# unigramcnt=load_obj('tmp.unigramdict')
+
+# trigramcnt[trigramcnt.keys()[0]] ## 1
+trigramcnt=load_obj('tmp.trigramcnt')
+
+# tdict[tdict.keys()[0]] ## ['106582840:-1977522313', '106582840:-2146217110', ...
+tdict=load_obj('tmp.trigramdict')
+
+trimi=[getTRIMI(tdict[x]) for x in tdict.keys()]
+
+n=0
+m=0
+#bad=set()
+bad=0
+
+for i in range(len(trimi)):
+    for j in range(len(trimi[i])):
+        m=m+1
+        if hashpagedict[tdict[tdict.keys()[i]][j].split(':')[0]] == '/test/me':
+            print trimi[i]
+        if trimi[i][j] < -20.6:
+            n=n+1
+            if hashpagedict[tdict[tdict.keys()[i]][j].split(':')[0]] == '/test/me':
+                bad=bad+1
+
+print bad,n,m,len(badpages)
+
+
+'''
---
+++ cs5860/src/getBigrams3.py
@@ -0,0 +1,135 @@
+﻿#http://codereview.stackexchange.com/questions/24276/correct-implementation-of-a-markov-chain
+import pandas as pd
+import numpy as np
+#import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+#from scipy.optimize import curve_fit
+from collections import Counter # 2.7
+import pickle
+import os.path
+
+def save_obj(obj, name ):
+    print("saving "+name)
+    with open('../data/e10/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    print("loading "+name)
+    with open('../data/e10/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+pdf = pd.read_csv("../data/tmp.e10",sep=" ",names=["src","page","pass"])
+# save dict of hashes to rediscover web pages
+pageset = set(pdf['page'])
+hashpagedict=dict([(str(hash(page)),page) for page in list(pageset)])
+pagehashdict=dict([(page,str(hash(page))) for page in list(pageset)])
+save_obj(hashpagedict,"tmp.hashpagedict")
+save_obj(pagehashdict,"tmp.pagehashdict")
+
+srcset = set(pdf['src'])
+hashsrcdict=dict([(str(hash(src)),src) for src in list(srcset)])
+srchashdict=dict([(src,str(hash(src))) for src in list(srcset)])
+save_obj(hashsrcdict,"tmp.hashsrcdict")
+save_obj(srchashdict,"tmp.srchashdict")
+
+# 
+badpages=list(set(pdf[pdf['pass']=='FAIL']['page']))
+save_obj(badpages,"tmp.badpages")
+
+
+pdf2=pdf.iloc[:,0:2]
+pdf2.src = [str(hash(p)) for p in pdf2.src]
+pdf2.page = [str(hash(p)) for p in pdf2.page]
+pdf2.src = pd.DataFrame(pdf2.src, dtype='str')
+pdf2.page = pd.DataFrame(pdf2.page, dtype='str')
+
+bypage=pdf2.groupby('page')
+pagecnt=bypage.count()
+unigramdict=pagecnt['src'].to_dict()
+for k in unigramdict.keys():
+    unigramdict[k]=int(unigramdict[k])
+
+save_obj(unigramdict,"tmp.unigramdict")
+
+def getBigrams(x):
+    tg=[]
+    for n in range(len(x)-1):
+         tg.append([str(x.iloc[n]),str(x.iloc[n+1])])
+    return(tg)
+
+def getTrigrams(x):
+    tg=[]
+    for n in range(len(x)-2):
+         tg.append([str(x.iloc[n]),str(x.iloc[n+1]),str(x.iloc[n+2])])
+    return(tg)
+
+# -1005790194    [[-863432790, 1535905453], [1535905453, -15011...
+bigrams=pdf2.groupby('src').page.apply(getBigrams)
+save_obj(bigrams,"tmp.bigrams")
+
+# [['-863432790', '1535905453'], ['1535905453', '-1501131867'], ...
+bflat=[item for sublist in list(bigrams) for item in sublist]
+save_obj(bflat,"tmp.bflat")
+
+# ['716482353:477775975', '477775975:716482353', '716482353:1862355765', ...]
+bigrams2=[':'.join([str(t[0]),str(t[1])]) for t in bflat]
+save_obj(bigrams2,"tmp.bigrams2")
+
+# Counter is the program
+bigramcnt=Counter(bigrams2)
+save_obj(bigramcnt,"tmp.bigramcnt")
+
+# ['544613893:-1825626037', '-1693265907:-170875555', '1527289180:-189339146']
+# bigramcnt.keys()[0:3]
+# [1, 1, 1]
+# bigramcnt.values()[0:3]
+sum(bigramcnt.values()) # 3315616 ## 31645
+len(bigramcnt.keys()) # 228180 ## 8867
+
+# given A, what are the possible A:B bigrams
+bigramdict={}
+for i in bigramcnt.keys():
+    a=i.split(':')[0]
+    try:
+        bigramdict[a]=bigramdict[a]+[i]
+    except:
+        bigramdict[a]=[i]
+
+save_obj(bigramdict,"tmp.bigramdict")
+
+# =========================================================================
+
+# -1005790194    [[-863432790, 1535905453, -1501131867], [15359...
+trigrams=pdf2.groupby('src').page.apply(getTrigrams)
+save_obj(trigrams,"tmp.trigrams")
+
+# [['-863432790', '1535905453', '-1501131867'], ['1535905453', '-1501131867', ...
+tflat=[item for sublist in list(trigrams) for item in sublist]
+save_obj(tflat,"tmp.tflat")
+
+# -1005790194    [[-863432790, 1535905453, -1501131867], [15359...
+trigrams2=[':'.join([str(t[0]),str(t[1]),str(t[2]),]) for t in tflat]
+save_obj(trigrams2,"tmp.trigrams2")
+
+# Counter is the program
+trigramcnt=Counter(trigrams2)
+save_obj(trigramcnt,"tmp.trigramcnt")
+
+# ['544613893:-1825626037', '-1693265907:-170875555', '1527289180:-189339146']
+# trigramcnt.keys()[0:3]
+# [1, 1, 1]
+# trigramcnt.values()[0:3]
+sum(trigramcnt.values()) # xxxx ## 29026
+len(trigramcnt.keys()) # xxxx ## 13933
+
+# given A, what are the possible A:B trigrams
+trigramdict={}
+for i in trigramcnt.keys():
+    a=':'.join(i.split(':')[0:2])
+    try:
+        trigramdict[a]=trigramdict[a]+[i]
+    except:
+        trigramdict[a]=[i]
+
+save_obj(trigramdict,"tmp.trigramdict")
